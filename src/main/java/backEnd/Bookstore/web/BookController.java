b06355bee4ac5a09aecfe63e11e77cbdcb953d38
bfm471 <bfm471@myy.haaga-helia.fi>
2023-09-06 22:06:09 +0300

    first commit

---
+++ src/main/java/backEnd/Bookstore/web/BookController.java
@@ -0,0 +1,14 @@
+package backEnd.Bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BookController {
+	
+	@GetMapping("/index")
+	public String getStarted() {
+		return("index");
+	}
+
+}
