732c92a9f73b7fabc5cff0a01e0d475ab45e6319
bfm471 <bfm471@myy.haaga-helia.fi>
2023-09-09 09:57:10 +0300

    Chapter 3 - tehtävä 2

--- src/main/java/backEnd/Bookstore/web/BookController.java
+++ src/main/java/backEnd/Bookstore/web/BookController.java
@@ -1,14 +1,27 @@
 package backEnd.Bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import backEnd.Bookstore.domain.BookRepository;
 
 @Controller
 public class BookController {
+	
+	@Autowired
+	private BookRepository repository;
 	
 	@GetMapping("/index")
 	public String getStarted() {
 		return("index");
 	}
 
+	@GetMapping("/booklist")
+	public String listBooks(Model model) {
+		model.addAttribute("books", repository.findAll());
+		return("booklist");
+	}
+	
 }
